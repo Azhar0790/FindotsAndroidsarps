60efd9507e89dc76747f7e49187ca9bedbac7341
Jomy Paulose <jpaulose@bridgetree.com>
2016-07-12 10:23:55 +0530

    Package change Update

--- mobile/src/main/java/restcalls/ForgotPassword/IForgotPasswordRestCall.java
+++
@@ -1,9 +0,0 @@
-package restcalls.forgotPassword;
-
-/**
- * Created by parijathar on 6/17/2016.
- */
-public interface IForgotPasswordRestCall {
-    public void onForgotPasswordSuccess(ForgotPasswordModel forgotPasswordModel);
-    public void onForgotPasswordFailure(String errorMessage);
-}
--- mobile/src/main/java/restcalls/Register/IRegisterRestCall.java
+++
@@ -1,9 +0,0 @@
-package restcalls.register;
-
-/**
- * Created by parijathar on 6/17/2016.
- */
-public interface IRegisterRestCall {
-    public void onRegisterUserSuccess(RegisterModel registerModel);
-    public void onRegisterUserFailure(String errorMessage);
-}
