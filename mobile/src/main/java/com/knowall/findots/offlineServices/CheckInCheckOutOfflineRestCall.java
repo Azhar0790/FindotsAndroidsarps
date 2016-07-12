35c5e0aa18cbf8e26140383678d5da31af2dcddf
Jomy Paulose <jpaulose@bridgetree.com>
2016-07-12 10:50:59 +0530

    Package change Update

---
+++ mobile/src/main/java/com/knowall/findots/offlineServices/CheckInCheckOutOfflineRestCall.java
@@ -0,0 +1,116 @@
+package com.knowall.findots.offlineServices;
+
+import android.content.Context;
+import android.util.Log;
+
+import com.knowall.findots.Constants;
+import com.knowall.findots.FinDotsApplication;
+import com.knowall.findots.database.DataHelper;
+import com.knowall.findots.database.dataModel.checkIn;
+import com.knowall.findots.events.AppEvents;
+import com.knowall.findots.restcalls.checkInCheckOut.CheckInCheckOutModel;
+import com.knowall.findots.utils.AppStringConstants;
+import com.knowall.findots.utils.GeneralUtils;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import de.greenrobot.event.EventBus;
+import retrofit.Call;
+import retrofit.Callback;
+import retrofit.Response;
+import retrofit.Retrofit;
+
+/**
+ * Created by jpaulose on 6/30/2016.
+ */
+public class CheckInCheckOutOfflineRestCall {
+
+    Context context = null;
+
+
+    public CheckInCheckOutOfflineRestCall(Context context) {
+        this.context = context;
+    }
+
+    public void callCheckInOutOfflineService(final boolean isCheckIn) {
+
+        Map<String, Object> postValues = getDestinationsRequest(isCheckIn);
+
+        Call<CheckInCheckOutModel> call = null;
+
+        if (!isCheckIn) {
+            call = FinDotsApplication.getRestClient().getApiService().checkIn(postValues);
+        } else {
+            call = FinDotsApplication.getRestClient().getApiService().checkOut(postValues);
+        }
+
+        call.enqueue(new Callback<CheckInCheckOutModel>() {
+            @Override
+            public void onResponse(Response<CheckInCheckOutModel> response, Retrofit retrofit) {
+
+                if (response.isSuccess()) {
+                    DataHelper dataHelper = DataHelper.getInstance(context);
+                    if (!isCheckIn) {
+                        EventBus.getDefault().post(AppEvents.OFFLINECHECKIN);
+                        Log.d("jomy","call Offline");
+                        dataHelper.deleteCheckinList();
+                    }
+                    else {
+                        EventBus.getDefault().post(AppEvents.OFFLINECHECKOUT);
+                        Log.d("jomy","call Offlineout");
+                        dataHelper.deleteCheckoutList();
+                    }
+                }
+            }
+
+            @Override
+            public void onFailure(Throwable t) {
+
+            }
+        });
+    }
+
+    private Map<String, Object> getDestinationsRequest(boolean isCheckIn) {
+
+        /**
+         *   fetch current lat and lng
+         */
+
+        DataHelper dataHelper = DataHelper.getInstance(context);
+        List<checkIn> checkInList;
+        if (!isCheckIn) {
+            checkInList = dataHelper.getCheckInListToSync();
+        } else
+            checkInList = dataHelper.getCheckOutListToSync();
+
+        List<Map<String, Object>> list = new ArrayList<>();
+
+        /**
+         *   fetch all the checkIn details from com.knowall.findots.database If any
+         *   and create request
+         */
+        for (checkIn checkInData : checkInList) {
+            Map<String, Object> checkInValues = new HashMap<>();
+            checkInValues.put("assignDestinationID", checkInData.getAssigned_destinationId());
+            checkInValues.put("reportedTime", checkInData.getReportedTime());
+            checkInValues.put("checkedInOutLatitude", checkInData.getCheckInOUTlatitude());
+            checkInValues.put("checkedInOutLongitude", checkInData.getCheckInOUTlongitude());
+            list.add(checkInValues);
+        }
+
+
+        int userID = GeneralUtils.getSharedPreferenceInt(context, AppStringConstants.USERID);
+
+        Map<String, Object> postValues = new HashMap<>();
+        postValues.put("checkedInsandOuts", list);
+        postValues.put("appVersion", GeneralUtils.getAppVersion(context));
+        postValues.put("deviceTypeID", Constants.DEVICETYPEID);
+        postValues.put("deviceInfo", GeneralUtils.getDeviceInfo());
+        postValues.put("userID", userID);
+        return postValues;
+    }
+
+}
---
+++ mobile/src/main/java/com/knowall/findots/restservice/RestClient.java
@@ -0,0 +1,65 @@
+package com.knowall.findots.restservice;
+
+import com.squareup.okhttp.Interceptor;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
+import com.squareup.okhttp.logging.HttpLoggingInterceptor;
+
+import java.io.IOException;
+
+import retrofit.GsonConverterFactory;
+import retrofit.Retrofit;
+
+/**
+ * Created by parijathar on 6/6/2016.
+ */
+public class RestClient {
+
+    private static final String ACCEPT_KEY = "Accept";
+    private static final String APPLICATION_JSON = "application/json";
+
+    private static final String AUTHORIZATION_KEY = "Authorization";
+    private static final String AUTH_TOKEN = "auth-token";
+
+    NetworkController apiService = null;
+
+    public RestClient() {
+
+        HttpLoggingInterceptor loggingInterceptor = new HttpLoggingInterceptor();
+        loggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+
+        OkHttpClient client = new OkHttpClient();
+        client.interceptors().add(loggingInterceptor);
+        client.interceptors().add(new Interceptor() {
+            @Override
+            public Response intercept(Chain chain) throws IOException {
+                Request original = chain.request();
+
+                Request request = original.newBuilder()
+                        .header(ACCEPT_KEY, APPLICATION_JSON)
+                        .header(AUTHORIZATION_KEY, AUTH_TOKEN)
+                        .method(original.method(), original.body())
+                        .build();
+
+                Response response = chain.proceed(request);
+
+                return response;
+            }
+        });
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(RestURLs.BASE_URL)
+                .client(client)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        apiService = retrofit.create(NetworkController.class);
+
+    }
+
+    public NetworkController getApiService() {
+        return apiService;
+    }
+
+}
