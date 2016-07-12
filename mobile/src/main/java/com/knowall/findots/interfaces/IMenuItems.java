fb0151d8b6eb5571c21fb2824cbf929b20173bfc
Jomy Paulose <jpaulose@bridgetree.com>
2016-07-12 10:49:40 +0530

    Package change Update

---
+++ mobile/src/main/java/com/knowall/findots/interfaces/IMenuItems.java
@@ -0,0 +1,8 @@
+package com.knowall.findots.interfaces;
+
+/**
+ * Created by parijathar on 5/26/2016.
+ */
+public interface IMenuItems {
+    public void onMenuItemSelected(int itemPosition);
+}
---
+++ mobile/src/main/java/com/knowall/findots/restmodels/responseDataModel.java
@@ -0,0 +1,16 @@
+package com.knowall.findots.restmodels;
+
+/**
+ * Created by jpaulose on 6/21/2016.
+ */
+public class responseDataModel {
+    private String status;
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
